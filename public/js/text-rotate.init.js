abe890b657d87877fd3ae011c65899921a680a9d
featitx <siddabdul7@gmail.com>
2024-10-25 23:37:21 +0530

    Add new deploy

---
+++ public/js/text-rotate.init.js
@@ -0,0 +1,11 @@
+
+/* Theme Name: Hiric - Responsive Landing page template
+   Author: Themesbrand
+   Version: 1.0.0
+   File Description: Text Rotate JS file
+*/
+
+    $(".text-rotate").textrotator({
+        animation: "fade",
+        speed: 1500
+    });
